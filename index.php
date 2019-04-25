eae538c3f3c41e025f275ece19dd43455718a27e
anderson Junior <andersonrodriguesjunior1@gmail.com>
2019-04-25 19:20:19 +0000

    modificaçao do projeto

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link rel="stylesheet" href="../css/bootstrap.css">
+  <link rel="stylesheet" href="../style.css">
+
+
+</head>
+<body>
+<div class="content ">
+  <div class="banner col-md-12 ">
+  <h2 class=" text-center"></h2>
+  </div>
+  <hr></hr>
+  <div class="container offset-1">
+  <div class="col-md-12 ">  
+    <div class="row  ">
+<button id="enfe" class="btn btn-danger col-md-4  my-2  rounded-left  ">Enfermagem</button>
+
+<button id="anal" class="btn  btn-primary col-md-4  my-2  ">Analise de sistemas</button>
+ 
+<button id="turis" class="btn btn-success   col-md-4  my-2  rounded-right  text-white">Turismo</button>
+ </div>
+ <div class="row ">
+<button id="edif" class="btn  btn-warning col-md-4  my-2 bg-warning  rounded">Edificações</button>
+<button id="edif" class="btn  btn-dark col-md-4  my-2   rounded">Manutenção </button>
+<button id="edif" class="btn  btn-light col-md-4  my-2 border-dark  rounded">Administração</button>
+</div>
+    </div>
+    </div>
+  </div>
+<button id="b5" class=" btn bt-lg btn-outline-danger" href="https://www.vestibulinhoetec.com.br/candidato/dados-acesso.asp"> inscrever-se <span id="sp"><img id=img-btn src="../etec.png"></img> </span></button>
+
+</body>
+</html>
